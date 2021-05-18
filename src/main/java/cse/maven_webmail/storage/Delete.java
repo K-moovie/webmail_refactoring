11cdb3167215da3a6328c783d16ca776fd90f636
yejin0528 <74442264+yejin0528@users.noreply.github.com>
2021-05-19 04:36:35 +0900

    Storage_mail

---
+++ src/main/java/cse/maven_webmail/storage/Delete.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cse.maven_webmail.storage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author lee-yejin
+ */
+@WebServlet(name = "Delete", urlPatterns = {"/Delete"})
+public class Delete extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+           final String JdbcDriver = "com.mysql.cj.jdbc.Driver";
+            final String JdbcUrl = "jdbc:mysql://localhost:3306/webmail?serverTimezone=Asia/Seoul";
+            final String User = "jdbctester";
+            final String Password = "4002017";
+            
+            try{
+                String subj = request.getParameter("delsubj");
+                
+                Class.forName(JdbcDriver);
+                Connection conn = DriverManager.getConnection(JdbcUrl,User,Password);
+                String sql = "DELETE FROM save WHERE subj='" + subj + "';" ;  //idx로 바꿔야함
+                
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                request.setCharacterEncoding("UTF-8");
+               
+                if (!(subj == null) && !subj.equals("")) {  //idx
+                    pstmt.executeUpdate();
+                }
+                
+                pstmt.close();
+                conn.close();
+                response.sendRedirect("storage_list.jsp");
+                 
+            } catch (Exception ex) {
+               out.println("오류가 발생했습니다. (발생 오류: " + ex.getMessage() + ")");
+            } finally{
+                out.close();
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/webapp/storage_list.jsp
@@ -0,0 +1,79 @@
+<%-- 
+    Document   : storage_list
+    Author     : lee-yejin
+--%>
+
+<%@page import="java.sql.ResultSet"%>
+<%@page import="java.sql.Statement"%>
+<%@page import="java.sql.DriverManager"%>
+<%@page import="java.sql.Connection"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>임시저장 메일 목록</title>
+        <link type="text/css" rel="stylesheet" href="css/main_style.css" />
+    </head>
+    <jsp:include page="header.jsp" />
+                
+         <%
+            final String JdbcDriver = "com.mysql.cj.jdbc.Driver";
+            final String JdbcUrl = "jdbc:mysql://localhost:3306/webmail?serverTimezone=Asia/Seoul";
+            final String User = "jdbctester";
+            final String Password = "4002017";
+            
+            try{
+            Class.forName(JdbcDriver);
+            Connection conn = DriverManager.getConnection(JdbcUrl,User,Password);
+            Statement stmt = conn.createStatement();
+            
+            String sql = "SELECT * FROM save" ;
+            ResultSet rs = stmt.executeQuery(sql);
+            %>
+            
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>수신</th>
+                        <th>제목</th>
+                    </tr>
+                </thead>
+                
+               <tbody>
+                    <%
+                        while(rs.next()){
+                     %>
+                        
+                        <tr>
+                        <td><%=rs.getString("recv") %> </td> 
+                        
+                        <td> <a href='write_mail.jsp?recv=<%=rs.getString("recv")%>
+                        &cc=<%=rs.getString("cc")%>
+                        &subj=<%=rs.getString("subj")%>
+                        &body=<%=rs.getString("body")%>
+                        &date=<%=rs.getString("date")%>'>
+                        <%= rs.getString("subj")%> </a>
+                        </td>
+                        
+                        <td>
+                             <form action ="Delete">
+                                <input type="hidden" name="delsubj" value="<%=rs.getString("subj")%>">
+                                <input type="submit" value="삭제">
+                             </form>
+                        </tr>                       
+                     <%  }
+                        rs.close();
+                        stmt.close();
+                        conn.close();
+                     %>
+                </tbody>
+            </table>
+                
+            <%
+                }catch(Exception ex){
+                    out.println("오류 발생:" + ex.getMessage()); }
+                %>
+    
+             <jsp:include page="footer.jsp" />
+</html>
